bf05c3e09463f9aebf256da2582a2aaba0eae038
Keith Morrison <keithm@infused.org>
2008-12-17 00:56:02 -0800

    Make userstamp columns configurable
      # Globally in environment.rb
      ActiveRecord::Base.created_userstamp_column = :creator_id
    
      # In a model definition
      class Subscription
        self.created_userstamp_column = :creator_id
        self.updated_userstamp_column = :updater_id
      end

--- lib/blame.rb
+++ lib/blame.rb
@@ -6,6 +6,12 @@ module Blame
       
       base.class_inheritable_accessor :record_userstamps, :instance_writer => false
       base.record_userstamps = true
+      
+      base.class_inheritable_accessor :created_userstamp_column, :instance_writer => false
+      base.created_userstamp_column = :created_by
+      
+      base.class_inheritable_accessor :updated_userstamp_column, :instance_writer => false
+      base.updated_userstamp_column = :updated_by
     end
     
     private
@@ -16,15 +22,15 @@ module Blame
     
     def create_with_userstamps
       if record_userstamps && userstamp_object
-        write_attribute('created_by', userstamp_object.id) if respond_to?(:created_by)
-        write_attribute('updated_by', userstamp_object.id) if respond_to?(:updated_by)
+        write_attribute(created_userstamp_column, userstamp_object.id) if respond_to?(created_userstamp_column)
+        write_attribute(updated_userstamp_column, userstamp_object.id) if respond_to?(updated_userstamp_column)
       end
       create_without_userstamps
     end
     
     def update_with_userstamps(*args)
       if record_userstamps && userstamp_object && (!partial_updates? || changed?)
-        write_attribute('updated_by', userstamp_object.id) if respond_to?(:updated_by)
+        write_attribute(updated_userstamp_column, userstamp_object.id) if respond_to?(updated_userstamp_column)
       end
       update_without_userstamps
     end
